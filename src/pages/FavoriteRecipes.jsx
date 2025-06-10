5b0d7c9f637e341950c85bb5cb3b396dd763a2f3
Amanda <rodriguesamanda49@gmail.com>
2025-06-10 17:00:09 -0300

    mediaQs for fav and done recipes page finished

--- src/pages/FavoriteRecipes.jsx
+++ src/pages/FavoriteRecipes.jsx
@@ -1,49 +1,48 @@
-import React, { useEffect, useState } from 'react';
-import CardDoneAndFavorite from '../components/CardDoneAndFavorite';
+import React, { useEffect, useState } from "react";
+import CardDoneAndFavorite from "../components/CardDoneAndFavorite";
 // import FiltersDoneAndFavorite from '../components/FiltersDoneAndFavorite';
-import Header from '../components/Header';
-import '../css/FavoriteRecipes.css';
-import '../css/CardDoneAndFavorite.css';
-import blackHeartIcon from '../images/blackHeartIcon.svg';
+import Header from "../components/Header";
+import "../css/FavoriteRecipes.css";
+import "../css/CardDoneAndFavorite.css";
+import blackHeartIcon from "../images/blackHeartIcon.svg";
 
 export default function FavoriteRecipes() {
   const [favoriteRecipes, setFavoriteRecipes] = useState([]);
 
   useEffect(() => {
-    const recipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
+    const recipes = JSON.parse(localStorage.getItem("favoriteRecipes"));
     setFavoriteRecipes(recipes);
   }, []);
 
   const disfavorRecipe = (id) => {
-    const favorites = JSON.parse(localStorage.getItem('favoriteRecipes'));
+    const favorites = JSON.parse(localStorage.getItem("favoriteRecipes"));
     const removeRecipe = favorites.filter((favorite) => favorite.id !== id);
-    localStorage.setItem('favoriteRecipes', JSON.stringify(removeRecipe));
+    localStorage.setItem("favoriteRecipes", JSON.stringify(removeRecipe));
     setFavoriteRecipes(removeRecipe);
   };
 
   return (
     <div className="favorites__page">
       <Header />
-      {/* <FiltersDoneAndFavorite setState={ setFavoriteRecipes } /> */}
-      { favoriteRecipes && favoriteRecipes.map((favoriteRecipe, index) => (
-        <div key={ index } className="card__done__favorite">
-          <CardDoneAndFavorite
-            recipe={ favoriteRecipe }
-            indexRecipe={ index }
-          />
-          <button
-            data-testid={ `${index}-horizontal-favorite-btn` }
-            onClick={ () => disfavorRecipe(favoriteRecipe.id) }
-            src={ blackHeartIcon }
-            className="favoriteCBtn"
-          >
-            <img
-              src={ blackHeartIcon }
-              alt="favorite"
-            />
-          </button>
-        </div>
-      ))}
+      <div className="doneFav-div">
+        {favoriteRecipes &&
+          favoriteRecipes.map((favoriteRecipe, index) => (
+            <div key={index} className="card__done__favorite">
+              <CardDoneAndFavorite
+                recipe={favoriteRecipe}
+                indexRecipe={index}
+              />
+              <button
+                data-testid={`${index}-horizontal-favorite-btn`}
+                onClick={() => disfavorRecipe(favoriteRecipe.id)}
+                src={blackHeartIcon}
+                className="favoriteCBtn"
+              >
+                <img src={blackHeartIcon} alt="favorite" />
+              </button>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
